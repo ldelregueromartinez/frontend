ac24cbe3fb3b2c21177f015dc130fc8f1170d7e6
Lucas del Reguero Martinez <ldelregueromartinez@siu.edu.ar>
2023-11-06 23:00:22 -0300

    avance

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Especialistas } from "./components/Especialistas";
+import { Consultorios } from "./components/Consultorios";
+import { Menu } from "./components/Menu";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+    return (
+    <BrowserRouter>
+    <Menu></Menu>
+
+    <div className="container">
+  
+      <div >
+        <h1 className="h1 text-center my-4">Rutas en React</h1>
+      </div>
+
+      <Routes>
+      <Route path="/consultorios" Component={Consultorios}></Route>
+
+      <Route path="/especialistas" Component={Especialistas}></Route>
+
+
+
+      </Routes>
+      </div>
+
+    </BrowserRouter>
+ )};
+
 
 export default App;
